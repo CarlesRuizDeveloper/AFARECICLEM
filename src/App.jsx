0d6ca2365787a29e353bf4bffd36504c43016f59
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-10-09 11:37:02 +0200

    Con mocks funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
 import BookDetail from './pages/BookDetails';
+import ChatList from './pages/ChatList';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/llibre/:id" element={<BookDetail />} />
+          <Route path="/chatlist/:id" element={<ChatList />} />
         </Routes>
       </main>
       <Footer /> 
