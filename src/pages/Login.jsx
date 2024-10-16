a9ae121369ab2047d11549b776672422e1a4b6c7
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-10-16 12:10:58 +0200

    Mejora en taildwin estilos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom'; 
 import axios from 'axios';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
@@ -66,25 +68,32 @@ const Login = () => {
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full p-2 bg-mediumRed text-gray-300 placeholder-gray-400 focus:border-none rounded"
+            className="w-full p-2 bg-mediumRed text-gray-300 placeholder-gray-300 focus:border-none rounded"
             placeholder="Correu electrònic"
             required
           />
         </div>
-        <div className="mb-4">
+        <div className="mb-4 relative">
           <input
-            type="password"
+            type={passwordVisible ? 'text' : 'password'}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-2 border bg-mediumRed border-gray-300 placeholder-gray-400 rounded"
+            className="w-full p-2 border bg-mediumRed border-gray-300 placeholder-gray-300 rounded pr-10"
             placeholder="Clau d'accés"
             required
           />
+          <button
+            type="button"
+            onClick={() => setPasswordVisible(!passwordVisible)}
+            className="absolute right-2 top-3 text-gray-300 focus:outline-none"
+          >
+            {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+          </button>
         </div>
 
         <button 
           type="submit" 
-          className="w-full bg-white text-darkRed font-bold p-2 rounded hover:bg-green-600 hover:text-white transition" 
+          className="w-full bg-white text-darkRed font-bold p-2 rounded hover:bg-gray-700 hover:text-white transition" 
           disabled={isLoading}
         >
           {isLoading ? 'Iniciant sessió...' : 'Inicia sessió'}
@@ -98,7 +107,7 @@ const Login = () => {
           <button
             type="button"
             onClick={() => navigate('/register')}
-            className="w-full mt-4 bg-red-300 text-gray-200 p-2 rounded font-bold hover:bg-green-600 transition"
+            className="w-full mt-4 bg-gray-700 text-gray-200 p-2 rounded font-bold hover:bg-white hover:text-black transition"
           >
             Registra't aquí!
           </button>
